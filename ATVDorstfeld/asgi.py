93b7a3569cb09e28beb521a49ed84ca8ae343892
rherrmannr <robinherrmann@hotmail.de>
2020-10-17 11:17:50 +0200

    initial contribution

---
+++ ATVDorstfeld/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ATVDorstfeld project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ATVDorstfeld.settings')
+
+application = get_asgi_application()
