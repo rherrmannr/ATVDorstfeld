559fa007738b7ad51612fca63fb3a787ee3b4d97
rherrmannr <robinherrmann@hotmail.de>
2020-10-18 14:50:05 +0200

    show all courts

--- courts/views.py
+++ courts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 
 from courts.model import Court
@@ -7,6 +8,6 @@ class CourtsListView(ListView):
     model = Court
     # model = Post -> wird zu court
     template_name = 'courts/home.html'
-    # context_object_name = 'posts'
+    context_object_name = 'courts'
 # ordering = ['-date_posted']
 # paginate_by = 5
