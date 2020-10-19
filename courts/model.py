f02dc320f7054bae22724752a379d1dbc6c82f5b
rherrmannr <robinherrmann@hotmail.de>
2020-10-19 14:29:45 +0200

    add double_field to db

--- courts/model.py
+++ courts/model.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Court(models.Model):
     title = models.CharField(max_length=100)
     used = models.BooleanField(default=False)
+    double_field = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
