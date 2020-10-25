c97a52f3bb56230a49030ebccc637d7334876942
rherrmannr <robinherrmann@hotmail.de>
2020-10-25 13:57:21 +0100

    added first_name and second_name as required attributes for registration

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.views.generic import CreateView
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True,)
+    last_name = forms.CharField(required=True,)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
